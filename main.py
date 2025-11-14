12d74de16f36a87afbd406fa6edfdfb887783815
sanmajmo <sanmajmo@gmail.com>
2025-11-14 15:04:30 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-def main():
-  print("Hello learners!")
+def addmultiplenumbers(numbers):
+    return sum(numbers)
 
-if __name__=="__main__":
-  main()
+
+def multiplymultiplenumbers(numbers):
+    result = 1
+    for n in numbers:
+        result *= n
+    return result
+
+
+def isiteven(num):
+    return isinstance(num, int) and num % 2 == 0
+
+
+def isitaninteger(num):
+    return isinstance(num, int)
+
+
+def main():
+    print("Hello learners!")
+
+
+if __name__ == "__main__":
+    main()
